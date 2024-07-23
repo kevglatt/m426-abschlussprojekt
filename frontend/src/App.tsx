33e9edd98865e50170041eb1bddb5ea899b3ab79
Kevin Glatthard <kevin.glatthard@gmail.com>
2024-07-23 18:41:21 +0200

    #12 implement deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {TaskModel} from "./models/task.model";
 import TaskCard from "./components/task-card";
@@ -7,17 +7,23 @@ import TaskCard from "./components/task-card";
 function App() {
 
     const [tasks, setTasks] = React.useState<TaskModel[]>([]);
+    const [error, setError] = useState(false);
     useEffect(() => {
         fetch("http://localhost:8080/tasks")
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 setTasks(data)
+            })
+            .catch(error => {
+                setError(true);
+                console.log(error)
             });
     }, []);
 
     return (
         <div className="App">
+            <div>{error && <p>Abschlussprojekt Kevin Glatthard Frontend</p>}</div>
             <div className={"max-w-md mx-auto px-lg-5"}>
                 {tasks.map(
                     task =>
